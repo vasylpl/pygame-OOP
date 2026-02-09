3bae1176aa80e8e1bb60d62556ca09525912ef43
Vasyl Polazhynets <Vasyl.Polazhynets@student.souepl.cz>
2026-02-09 13:44:47 +0100

    pygame

---
+++ game/platform.py
@@ -0,0 +1,11 @@
+import  pygame
+from config import *
+
+class Platform(pygame.sprite.Sprite):
+    def __init__(self, x, y, width, height):
+        super().__init__()
+        self.image = pygame.Surface((width, height))
+        self.image.fill(PLATFORM_COLOR)
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
